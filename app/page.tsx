f058a20f1dc747e7c548a479bbb740961755ee7f
Zainab <abdulrasaqzainab76@gmail.com>
2025-03-04 22:09:43 +0100

    feat: add Features section with insights and reporting UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import HomePage from "./(guest)/home";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "AI-Powered Financial Reconciliation System",
-  description:
-    "AI-Powered Financial Reconciliation in Minutes, Not Hours. Automate, compare, and reconcile transactions effortlessly with AI. No more manual matching—get accurate results in seconds.",
-};
+import FeaturesSection from './components/FeaturesSection';
 export default function Home() {
-  return <HomePage />;
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+   <FeaturesSection />
+
+    </div>
+  );
 }
