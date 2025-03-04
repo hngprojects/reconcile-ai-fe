16fbd3b40625fe1b67ccf3b4c73630af739eb375
Zainab <abdulrasaqzainab76@gmail.com>
2025-03-04 23:06:57 +0100

    feat: move FeaturesSection to home.tsx and revert changes in page.tsx

--- app/(guest)/home.tsx
+++ app/(guest)/home.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
+import FeaturesSection from "@/components/FeaturesSection";
 
 export default function HomePage() {
   return (
@@ -30,6 +31,7 @@ export default function HomePage() {
           className="mt-10"
         />
       </div>
+      <FeaturesSection />
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import FeaturesSection from './components/FeaturesSection';
-export default function Home() {
-  return (
-    <div className='flex justify-center items-center min-h-screen'>
-   <FeaturesSection />
+import HomePage from "./(guest)/home";
+import { Metadata } from "next";
 
-    </div>
-  );
+export const metadata: Metadata = {
+  title: "AI-Powered Financial Reconciliation System",
+  description:
+    "AI-Powered Financial Reconciliation in Minutes, Not Hours. Automate, compare, and reconcile transactions effortlessly with AI. No more manual matching—get accurate results in seconds.",
+};
+export default function Home() {
+  return <HomePage />;
 }
