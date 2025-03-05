41301bd3933f24b9f9b75fcc7d8199a32c969e3a
Oni Ismail Modupe <93334212+ismailoni@users.noreply.github.com>
2025-03-05 14:44:47 +0100

    fix: feature/navbar links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -17,32 +17,32 @@ const Nav = () => {
     <nav className="flex justify-between items-center py-6 px-10 border-b-[0.5px] border-[#0000001A]">
       <Logo />
       <div className="flex gap-2">
-        <Button className="bg-white border-2 border-[#2E604A] text-[#2E604A] hover:text-white hidden md:block">
-          <Link href='https://reconxi.com/'>
+        <Link href='https://reconxi.com/'>
+          <Button className="bg-white border-2 border-[#2E604A] text-[#2E604A] hover:text-white hidden md:block">
             Login
-          </Link>
-        </Button>
-        <Button className="bg-[#2E604A] text-white hidden md:block">
-          <Link href='https://reconxi.com/'>
+          </Button>
+        </Link>        
+        <Link href='https://reconxi.com/'>
+          <Button className="bg-[#2E604A] text-white hidden md:block">
             Sign Up
-          </Link>
-        </Button>
+          </Button>
+        </Link>
         <button onClick={toggleMenu} className="md:hidden">
           <MenuIcon size={24} />
         </button>
       </div>
       {isOpen && (
         <div className="absolute top-16 right-10 bg-white shadow-md rounded-md p-4 flex flex-col gap-2 md:hidde w-[70%]">
-          <Button className="bg-white border-2 border-[#2E604A] text-[#2E604A] hover:text-white">
-           <Link href='https://reconxi.com/'>
-            Login
+          <Link href='https://reconxi.com/'>
+            <Button className="bg-white border-2 border-[#2E604A] text-[#2E604A] hover:text-white hidden md:block">
+              Login
+            </Button>
+          </Link> 
+          <Link href='https://reconxi.com/'>
+            <Button className="bg-[#2E604A] text-white hidden md:block">
+              Sign Up
+            </Button>
           </Link>
-          </Button>
-          <Button className="bg-[#2E604A] text-white">
-           <Link href='https://reconxi.com/'>
-            Sign Up
-          </Link>
-          </Button>
         </div>
       )}
 
