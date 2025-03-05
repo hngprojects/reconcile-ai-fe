fef52c945d7ca0192f6aeff965f8092bed5de02a
Oni Ismail Modupe <ismailoni80@gmail.com>
2025-03-05 01:08:48 +0100

    Add feature: navbar

--- app/(guest)/home.tsx
+++ app/(guest)/home.tsx
@@ -1,10 +1,12 @@
 import Feature2 from "@/components/Feature2";
 import HeroSection from "@/components/hero-section";
+import Nav from "@/components/Nav";
 import React from "react";
 
 export default function HomePage() {
   return (
     <main>
+      <Nav />
       <HeroSection />
       <Feature2 />
     </main>
