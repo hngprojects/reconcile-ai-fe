db64999073ee75e06e84f87d7524681cf78689f2
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-03-05 13:17:39 +0100

    feat: add landing page CTA section
    ### Checklist
    
    - import CTASection component to the Home Page

--- app/(guest)/home.tsx
+++ app/(guest)/home.tsx
@@ -1,24 +1,20 @@
-
-
-
 import React from "react";
 import Feature2 from "@/components/Feature2";
 import HeroSection from "@/components/hero-section";
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 import FeaturesSection from "@/components/FeaturesSection";
+import CTASection from "@/components/CTASection";
 
 export default function HomePage() {
   return (
-
-
     <main>
       <Nav />
       <HeroSection />
       <Feature2 />
       <FeaturesSection />
+      <CTASection />
       <Footer />
     </main> 
-
   );
 }
