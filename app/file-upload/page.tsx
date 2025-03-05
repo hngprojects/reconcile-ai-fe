c01bfb0c3e16bc9ba6af64a23c9cd9f60f1a93ac
abdulAjayi <abdussomad8720@gmail.com>
2025-03-05 09:56:40 -0800

    feat:implement file upload screen

--- app/file-upload/page.tsx
+++ app/file-upload/page.tsx
@@ -3,43 +3,42 @@ import Image from 'next/image';
 const UploadFile = () => {
     
     return ( 
-        <div>
-        <div className="mx-20 xl:mx-36 2xl:mx-60 mt-5 flex-col [@media(min-width:900px)]:flex md:flex-row gap-x-10 [@media(min-width:900px)]:justify-between [@media(min-width:400px)]:space-y-10 [@media(min-width:900px)]:items-stretch mb-12">
-            <div className='h-full border border-[#dddddd] px-12 py-6 rounded-[18.22px]'>
-                <div className=']'>
-                    <h1 className='text-[#333333] font-semibold text-lg  leading-[140%]'>Upload Financial Statement</h1>
+        <div className='lg:px-[80px] lg:py-[60px] md:px-[30px] md:py-[30px] px-[24px] py-[15px]'>
+        <div className="grid-cols-1 md:grid md:grid-cols-2 lg:gap-x-10 mb-12 md:mb-[69px] space-y-5 md:gap-x-5">
+            <div className='h-full border border-[#dddddd] lg:px-[47px] px-[25px] py-[20px] md:px-[28px] rounded-[18.22px]'>
+                <div className='h-full flex flex-col justify-between'>
+                    <h1 className='text-[#333333] font-semibold md:text-2xl  md:leading-[33.6px] text-[16px] leading-[22.4px]'>Upload Bank Statement</h1>
+                    <div>
+                    <div className='text-center my-6 border-2 border-[#dddddd] border-opacity-[20px] flex justify-center items-center flex-col space-y-5 rounded-[12px] border-dotted border-[#dddddd'>
+                        <div className='mt-10'>
+                    <Image src="/assets/images/icon-upload.svg" alt="Upload Icon" width={40} height={40}/>
+                        </div>
+                    <p className='text-[#678e82] font-bold md:text-[18px] md:leading-[25.2px] text-[14px] leading-[19.6px] mb-5'>upload file</p>
+                    <p className='text-[#678e82] w-[150px] text-[16px] font-normal leading-[22.4px] md:text-[20px] md:leading-[28px] border-b border-[#678e82] mb-[51px]'>choose file here</p>
+                    </div>
+                    <p className='text-[#678e82] font-light leading-[140%] text-[14px]'>Supported formats: CSV</p>
+                    </div>
+                </div>
+            </div>
+            <div className='h-full border border-[#dddddd] lg:px-[47px] py-[20px] md:px-[28px] rounded-[18.22px] px-[25px]'>
+                <div className='h-full flex flex-col justify-between'>
+                    <h1 className='text-[#333333] font-semibold md:text-2xl  md:leading-[33.6px] text-[16px] leading-[22.4px]'>Upload Company's Ledger</h1>
+                    <div>
                     <div className='text-center my-6 border-2 border-[#dddddd] flex justify-center items-center flex-col space-y-5 rounded-[12px] border-dotted border-[#dddddd'>
                         <div className='mt-10'>
                     <Image src="/assets/images/icon-upload.svg" alt="Upload Icon" width={40} height={40}/>
                         </div>
-                    <p className='text-[#678e82] font-bold text-[20px] mb-5'>upload file</p>
-                    <p className='text-[#678e82] w-[150px] leading-[140%] border-b border-[#678e82] mb-[51px]'>choose file here</p>
+                    <p className='text-[#678e82] font-bold md:text-[18px] md:leading-[25.2px] text-[14px] leading-[19.6px] mb-5'>upload file</p>
+                    <p className='text-[#678e82] w-[150px] text-[16px] font-normal leading-[22.4px] md:text-[20px] md:leading-[28px] border-b border-[#678e82] mb-[51px]'>choose file here</p>
                     </div>
-                    <div className='flex justify-between items-center flex-nowrap gap-x-5 w-full'>
-                        <p className='text-[#678e82] font-light leading-[140%] text-[14px]'>Supported formats: CVS, XLS, PDF</p>
-                        <p className='leading-[140%] text-[14px] text-[#678e82] font-light'>Maximum size: 15MB</p>
+                        <p className='text-[#678e82] font-light leading-[140%] text-[14px]'>Supported formats: CSV</p>
                     </div>
                 </div>
             </div>
-            <div className='h-full border border-[#dddddd] border-dotted px-12 py-6 rounded-[18.22px]'>
-                <div className=''>
-                    <h1 className=' text-[#333333] font-semibold text-lg leading-[140%]'>Upload Company’s Ledger</h1>
-                    <div className='text-center my-6 border-[#dddddd] flex justify-center items-center flex-col space-y-5 rounded-[12px] border-2 border-dotted '>
-                        <div className='mt-10'>
-                    <Image src="/assets/images/icon-upload.svg" alt="Upload Icon" width={40} height={40}/>
-                        </div>
-                    <p className='text-[#678e82] font-bold text-[20px] mb-5'>upload file</p>
-                    <p className='text-[#678e82] w-[150px] leading-[140%] border-b border-[#678e82] mb-[51px]'>choose file here</p>
-                    </div>
-                    <div className='flex justify-between items-center flex-nowrap gap-x-5 w-full'>
-                        <p className='text-[#678e82] font-light leading-[140%] text-[14px]'>Supported formats: CVS, XLS, PDF</p>
-                        <p className='leading-[140%] text-[14px] text-[#678e82] font-light'>Maximum size: 15MB</p>
-                    </div>
-                </div>
-            </div>
+            
         </div>
         <div className='flex justify-center'>
-            <button className='text-[16px] text-white font-bold bg-[#297b65] rounded-[10px] p-2.5 text-center h-[50px] w-[316px] opacity-60'>Upload Documents</button>
+            <button className='text-[16px] text-white font-bold bg-[#9fb6ac] rounded-[10px] p-2.5 text-center h-[50px] w-[316px] opacity-60'>Upload Documents</button>
         </div>
         </div>
      );
