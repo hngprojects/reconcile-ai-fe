dddf245c5356e1091972610e72cbe1d28a42371a
Muogbo Emmanuel <muogboemmanuel9@gmail.com>
2025-03-05 20:01:44 +0100

    feat(FAQAccordion): enhance responsiveness and improve code indentation

--- components/FAQs/FAQSection.tsx
+++ components/FAQs/FAQSection.tsx
@@ -24,7 +24,7 @@ const faqItems: FAQ[] = [
   },
   {
     question: "Can I use ReconXi for personal finances?",
-    answer: "Absolutely! Your privacy and security are our top priorities. All data uploaded to Reconxi is encrypted and securely processed using industry-standard encryption protocols. We do not share your data with third parties and comply with strict data protection standards."
+    answer: "Absolutely! Your privacy and security are our top priorities. All data uploaded to ReconXi is encrypted and securely processed using industry-standard encryption protocols. We do not share your data with third parties and comply with strict data protection standards."
   },
   {
     question: "Will ReconXi integrate with other financial software (e.g., QuickBooks, Xero)?",
@@ -34,8 +34,8 @@ const faqItems: FAQ[] = [
 
 const FAQSection = () => {
   return (
-    <section className="min-w-[100%] p-[30px] sm:px-[50px] md:px-[100px] lg:px-[200px] xl:px-[350px] justify-center py-[96px]">
-      <div className="gap-[8px]">
+    <section className="px-5 sm:px-[50px] md:px-[150px] lg:px-[270px] py-[96px]">
+      <div className="w-full">
         <div className="flex flex-col gap-[20px] text-center">
           <h1 className="font-inter text-[#101828] font-semibold text-[36px] leading-[44px] text-gray-900 my-0">
             Frequently Asked Questions
@@ -45,13 +45,11 @@ const FAQSection = () => {
           </p>
         </div>
         <div className="my-[64]">
-        <FAQAccordion  faqs={faqItems} />
-
+          <FAQAccordion faqs={faqItems} />
         </div>
       </div>
     </section>
   );
 };
-
 
 export default FAQSection;
