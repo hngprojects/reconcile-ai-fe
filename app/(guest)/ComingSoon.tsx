4c03a00af5edfb31a7e263389cb04f7580236615
Joel Amaefula <joelamaefula@gmail.com>
2025-03-05 02:46:53 +0100

    refactor: modified routes of root and landing page

---
+++ app/(guest)/ComingSoon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function ComingSoon() {
+    return (
+        <div >
+            <div className="nav flex justify-start px-[40px] py-[25px] ">
+                <div className="nav__logo">
+                    {/* <Image 
+                    
+                    src=".\assets\images\Logo.png" 
+                    width={159}
+                    height={50}
+                    alt="company logo" /> */}
+
+                    <h2>Reconcile ai</h2>
+                </div>
+            </div>
+
+            {/* body */}
+
+        </div>
+    );
+}
