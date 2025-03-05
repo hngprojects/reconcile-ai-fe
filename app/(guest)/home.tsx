9f04be42a116f4e00340fcf1239ecc93d4b17cba
itzfahadoooo <foyindolapo@gmail.com>
2025-03-05 14:37:37 +0100

    fix: resolving merge conflicts

--- app/(guest)/home.tsx
+++ app/(guest)/home.tsx
@@ -5,9 +5,9 @@ import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 
 import FAQSection from "@/components/FAQs/FAQSection";
-import Features1 from "@/components/features1";
 import CTASection from "@/components/CTASection"
 import Featurex from "@/components/Featurex";
+import Features1 from "@/components/features1";
 
 export default function HomePage() {
   return (
