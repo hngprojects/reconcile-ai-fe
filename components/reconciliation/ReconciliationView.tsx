d6974689cdf135da33aeea7372a089cf382b6444
vixs101 <elijahvix695@gmail.com>
2025-03-06 05:47:19 +0100

    fix(footer): add missing privacy policy link

--- components/reconciliation/ReconciliationView.tsx
+++ components/reconciliation/ReconciliationView.tsx
@@ -3,9 +3,18 @@
 import { useMedia } from "react-use";
 import { MobileReconciliationView } from "./MobileReconciliationView";
 import { ReconciliationTable } from "./ReconciliationTable";
+import Header from "@/app/coming-soon/components/Header";
 
 export function ReconciliationView() {
   const isMobile = useMedia("(max-width: 768px)");
 
-  return isMobile ? <MobileReconciliationView /> : <ReconciliationTable />;
+  return isMobile ? 
+  <>
+  <Header />
+  <MobileReconciliationView />
+  </> :
+   <>
+    <Header />
+    <ReconciliationTable />
+   </>;
 }
