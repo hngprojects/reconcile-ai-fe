d2c13cd47912bc5762445680e86a99a86fa6073c
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-03-07 14:03:03 +0100

    feat: update navbar to the root layout and clean up

--- components/reconciliation/ReconciliationView.tsx
+++ components/reconciliation/ReconciliationView.tsx
@@ -3,18 +3,9 @@
 import { useMedia } from "react-use";
 import { MobileReconciliationView } from "./MobileReconciliationView";
 import { ReconciliationTable } from "./ReconciliationTable";
-import Header from "@/app/coming-soon/components/Header";
 
 export function ReconciliationView() {
   const isMobile = useMedia("(max-width: 768px)");
 
-  return isMobile ? 
-  <>
-  <Header />
-  <MobileReconciliationView />
-  </> :
-   <>
-    <Header />
-    <ReconciliationTable />
-   </>;
+  return isMobile ? <MobileReconciliationView /> : <ReconciliationTable />;
 }
