d6de2518c35f9c9e7f09217dfe05962b2a998f10
Dagger <odidihope@gmail.com>
2025-03-07 12:02:59 +0100

    fix: fixed lint unused vars error

--- components/reconciliation/upload/UploadModal.tsx
+++ components/reconciliation/upload/UploadModal.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogTitle, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Progress } from "@/components/ui/progress";
 import Image from "next/image";
 import starIcon from "@/public/star-icon.png";
