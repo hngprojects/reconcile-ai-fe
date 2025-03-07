f1247de68c1b0749c41b8daca76a7761d845ec91
SGTTW <88731480+SGTTW@users.noreply.github.com>
2025-03-07 06:19:42 +0100

    fixed file upload page

--- components/reconciliation/LedgerUpload.tsx
+++ components/reconciliation/LedgerUpload.tsx
@@ -1,23 +1,42 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import FileComponent from "./FileComponent";
 import FileUploaded from "./FileUploaded";
+import Image from "next/image";
 
-const LedgerUpload = ({
-  title,
-  ledger,
-}: {
+interface LedgerUploadProps {
   title: string;
   ledger: boolean;
+  setLedger: (value: boolean) => void;
+  ledgerFile: string;
+  setLedgerFile: (value: string) => void;
+}
+
+const LedgerUpload: React.FC<LedgerUploadProps> = ({
+  title,
+  ledger,
+  setLedger,
+  ledgerFile,
+  setLedgerFile,
 }) => {
   return (
     <div className="w-[620px] rounded-xl py-[16px] md:py-[10px] px-[24px] flex flex-col gap-[24px] justify-center max-w-[450px] border-[1.2px] border-[#333]">
       <h2 className="text-[24px] font-semibold">{title}</h2>
       {!ledger ? (
-        <FileComponent />
+        <FileComponent
+          onFileUploaded={(fileName) => {
+            setLedgerFile(fileName);
+            setLedger(true);
+          }}
+        />
       ) : (
-        <FileUploaded document="Company Ledger.csv" />
+        <FileUploaded
+          document={ledgerFile}
+          onDelete={() => {
+            setLedger(false);
+            setLedgerFile("");
+          }}
+        />
       )}
       <div className="w-full flex flex-col md:flex-row justify-between text-gray-[#333]">
         <small>Supported format: CSV</small>
