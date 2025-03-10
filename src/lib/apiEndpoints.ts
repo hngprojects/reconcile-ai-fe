bfa7014f3b9a1e44f0e34d7644500baec110fb50
Obiekwe Agnes-Mary Chinaza <50022245+chinazaaa@users.noreply.github.com>
2025-03-10 17:34:50 +0000

    Update apiEndpoints.ts

--- src/lib/apiEndpoints.ts
+++ src/lib/apiEndpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_API_URL}`;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
 if (!BASE_URL) {
   throw new Error("NEXT_PUBLIC_BASE_API_URL is not defined");
