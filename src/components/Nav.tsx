a5f7b82060308c516e7c6b610b86fe2ea72239d0
Zainab <abdulrasaqzainab76@gmail.com>
2025-03-20 15:47:09 +0100

    add link to the navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -48,6 +48,9 @@ const Nav = () => {
               <DropdownMenuItem>
                 <Link className={cn("font-medium text-[#333333]", pathname === "/enterprise" && "text-[#2E604A]")} href="/enterprise">Enterprise</Link>
               </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link className={cn("font-medium text-[#333333]", pathname === "/financial-pro" && "text-[#2E604A]")} href="/enterprise">Financial Provision</Link>
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
           </div>
