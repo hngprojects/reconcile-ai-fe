5949987894d545469ea0a9bebd324642797e149b
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-03-20 22:18:57 +0100

    feature: hide export button to user without paid plan and fix export button

--- src/components/revamped-reconciliation/context/ReconciliationProvider.tsx
+++ src/components/revamped-reconciliation/context/ReconciliationProvider.tsx
@@ -16,13 +16,12 @@ import {
   StatementWithScore,
   LedgerWithScore,
 } from "../types/frontendResponseTypes";
-// import { revertToBackendFormat } from "../helpers/revertBackToBackendFormat";
 import { updateReconciliation, fetchReconciliation } from "@/src/lib/api";
 import { ManualRequestBody } from "@/src/types/reconciliation";
 import { toast } from "sonner";
 import { transformReconciliationData } from "../helpers/transformReconciliationData";
 import { useAuth } from "@/src/components/context/AuthContext";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 interface ReconciliationContextProps {
   data: ReconciliationResponse;
@@ -88,12 +87,12 @@ export function ReconciliationProvider({ children }: { children: ReactNode }) {
   const [isLoading, setIsLoading] = useState(false);
   const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 10 });
   const [data, setData] = useState<ReconciliationResponse>(
-    {} as ReconciliationResponse,
+    {} as ReconciliationResponse
   );
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedRow, setSelectedRow] = useState<ReconciliationItem | null>(
-    null,
+    null
   );
   const path = usePathname();
 
@@ -101,34 +100,34 @@ export function ReconciliationProvider({ children }: { children: ReactNode }) {
     /* const reconciliationData = transformReconciliationData(
       dummyBackendResponseData
     ); */
-   const reconciliationId = path.split('/')[2];
-   console.log(reconciliationId);
-   const fetch = async() => {
-     try {
+    const reconciliationId = path.split("/")[2];
+    console.log(reconciliationId);
+    const fetch = async () => {
+      try {
         const response = await fetchReconciliation(reconciliationId as string);
 
-        if(response.success){
-           const reconciliationData = transformReconciliationData(response.data);
-           console.log({ reconciliationData });
+        if (response.success) {
+          const reconciliationData = transformReconciliationData(response.data);
+          console.log({ reconciliationData });
 
-            setData(reconciliationData);
+          setData(reconciliationData);
         }
-     } catch (e) {
-        console.error('Error: ', e);
-     }
-   }
+      } catch (e) {
+        console.error("Error: ", e);
+      }
+    };
     fetch();
   }, [path]); // Removed `data` from dependency array to prevent infinite re-rendering
 
   const reconciliationData = useMemo(
     () => data.reconciliation_data ?? [],
-    [data],
+    [data]
   );
 
   const paginatedData = useMemo(() => {
     return reconciliationData.slice(
       pagination.pageIndex * pagination.pageSize,
-      (pagination.pageIndex + 1) * pagination.pageSize,
+      (pagination.pageIndex + 1) * pagination.pageSize
     );
   }, [reconciliationData, pagination.pageIndex, pagination.pageSize]);
 
@@ -169,7 +168,7 @@ export function ReconciliationProvider({ children }: { children: ReactNode }) {
       const reconciliationId = data.reconciliation_id;
       const response = await updateReconciliation(
         reconciliationId,
-        body as ManualRequestBody,
+        body as ManualRequestBody
       );
 
       if (response.status !== "success") {
@@ -181,7 +180,7 @@ export function ReconciliationProvider({ children }: { children: ReactNode }) {
       const reconciliationData = transformReconciliationData(response.data);
       localStorage.setItem(
         "reconciliation",
-        JSON.stringify(reconciliationData),
+        JSON.stringify(reconciliationData)
       );
       setData(reconciliationData);
 
@@ -216,7 +215,7 @@ export function ReconciliationProvider({ children }: { children: ReactNode }) {
       const reconciliationId = data.reconciliation_id;
       const response = await updateReconciliation(
         reconciliationId,
-        body as ManualRequestBody,
+        body as ManualRequestBody
       );
 
       if (response.status !== "success") {
@@ -228,7 +227,7 @@ export function ReconciliationProvider({ children }: { children: ReactNode }) {
       const reconciliationData = transformReconciliationData(response.data);
       localStorage.setItem(
         "reconciliation",
-        JSON.stringify(reconciliationData),
+        JSON.stringify(reconciliationData)
       );
       setData(reconciliationData);
 
@@ -299,7 +298,7 @@ export const useReconciliation = () => {
 
   if (!context) {
     throw new Error(
-      "useReconciliation must be used within a ReconciliationProvider",
+      "useReconciliation must be used within a ReconciliationProvider"
     );
   }
 
