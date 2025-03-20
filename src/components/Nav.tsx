8dcef415a670f1e21f45e3b31f5f9f38d8f12044
IdrisRoti <idrisomisakin@gmail.com>
2025-03-20 09:58:28 +0100

    feat:add navbar links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,8 +6,20 @@ import { LogoIcon } from "./Icon/Icons";
 import UserAction from "./UserAction";
 import { usePathname } from 'next/navigation'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/src/components/ui/dropdown-menu"
+import { ChevronDown } from "lucide-react";
+
+import { cn } from "../lib/utils";
+
 const Nav = () => {
   const pathname = usePathname();
+
+  console.log("pathname", pathname)
 
   return (
     <nav className="border-b-[1px] flex items-center border-[#0000001A] sticky top-0 left-0 right-0 bg-white z-50">
@@ -20,6 +32,28 @@ const Nav = () => {
             </span>
           </div>
         </Link>
+
+        <nav className="flex items-center gap-6">
+          <Link className={cn("font-medium text-[#333333]", pathname === "/" && "text-[#2E604A]")} href="/">Home</Link>
+          <div className="">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center gap-2 cursor-pointer">
+              <span>Industries</span>
+              <ChevronDown className="w-4" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="text-[#333333]">
+              <DropdownMenuItem>
+                <Link className={cn("font-medium text-[#333333]", pathname === "/small-business" && "text-[#2E604A]")} href="/small-business">Small Business owner</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link className={cn("font-medium text-[#333333]", pathname === "/enterprise" && "text-[#2E604A]")} href="/enterprise">Enterprise</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          </div>
+          <Link className={cn("font-medium text-[#333333]", pathname === "/blog" && "text-[#2E604A]")} href="/blog">Blog</Link>
+          <Link className={cn("font-medium text-[#333333]", pathname === "/pricing" && "text-[#2E604A]")} href="/pricing">Pricing</Link>
+        </nav>
         
         { pathname !== "/demo" && (<UserAction/>)}
       </Container>
