c4c55b85a58f872b9f19e410bcef18c0182cb734
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-03-25 10:59:29 +0100

    remove "..." from Get Started For Free" CTA button

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -38,12 +38,12 @@ export default function HeroSection() {
               aria-label={
                 isAuthenticated
                   ? "Access Your Dashboard"
-                  : "Get Started For Free..."
+                  : "Get Started For Free"
               }
               text={
                 isAuthenticated
                   ? "Access Your Dashboard"
-                  : "Get Started For Free..."
+                  : "Get Started For Free"
               }
               className="bg-[#297B65] py-2 px-4 rounded-md font-semibold justify-center items-center h-12 w-full sm:w-64 text-sm text-white hover:bg-[#297B65]/90 flex cursor-pointer mr-auto"
             />
