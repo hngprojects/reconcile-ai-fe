59e2910a010f2d34b061c446978c9f389049e16a
Dagger <odidihope@gmail.com>
2025-03-25 16:28:46 +0100

    chore: making dashboard a protected route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,13 @@
+import ProtectedRoute from "@/src/components/auth/ProtectedRoute";
 import Container from "@/src/components/Container";
 import { Dashboard } from "@/src/components/dashboard/Dashboard";
 
 export default function DashboardPage() {
   return (
-    <Container className="my-8">
-      <Dashboard />
-    </Container>
+    <ProtectedRoute>
+      <Container className="my-8">
+        <Dashboard />
+      </Container>
+    </ProtectedRoute>
   );
 }
