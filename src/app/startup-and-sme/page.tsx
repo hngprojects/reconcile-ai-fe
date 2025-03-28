6c2b53ee93b2d6bfbd00245babae316e2f75e2a8
Praise Afolabi <praizafolabi@gmail.com>
2025-03-28 03:39:59 +0100

    feat: created startup and sme pages

---
+++ src/app/startup-and-sme/page.tsx
@@ -0,0 +1,47 @@
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+// Dynamic imports
+const HeroSection = dynamic(
+  () => import("@/src/components/startup-and-sme/hero-sect")
+);
+const Benefits = dynamic(
+  () => import("@/src/components/startup-and-sme/Benefits"),
+);
+const VideoFeature = dynamic(
+  () => import("@/src/components/startup-and-sme/VideoFeature")
+);
+const WhyReconXi = dynamic(
+  () => import("@/src/components/startup-and-sme/WhyReconXi")
+);
+const StartReconcile = dynamic(
+  () => import("@/src/components/startup-and-sme/Ready")
+);
+const FAQSection = dynamic(() => import("@/src/components/FAQs/FAQSection"));
+const Footer = dynamic(() => import("@/src/components/Footer"));
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen">
+      <Suspense fallback={<div className="min-h-[200px]" />}>
+        <HeroSection />
+      </Suspense>
+      <Suspense fallback={<div className="min-h-[200px]" />}>
+        <WhyReconXi />
+      </Suspense>
+      <Suspense fallback={<div className="min-h-[200px]" />}>
+        <StartReconcile />
+      </Suspense>
+      <Suspense fallback={<div className="min-h-[200px]" />}>
+        <VideoFeature />
+      </Suspense>
+      <Suspense fallback={<div className="min-h-[200px]" />}>
+        <Benefits />
+      </Suspense>
+      <Suspense fallback={<div className="min-h-[200px]" />}>
+        <FAQSection />
+      </Suspense>
+      <Footer />
+    </main>
+  );
+}
