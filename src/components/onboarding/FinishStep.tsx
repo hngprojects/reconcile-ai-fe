ea91f6a22893e9a0fc106fda33e44685aa753507
Zainab <abdulrasaqzainab76@gmail.com>
2025-04-02 15:30:54 +0100

    feat: add onboarding page

---
+++ src/components/onboarding/FinishStep.tsx
@@ -0,0 +1,110 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { Button } from '../ui/button'
+
+export default function FinishStep() {
+  const router = useRouter()
+
+  return (
+    <div className="mt-7 flex w-full flex-col items-center">
+      <div className="mb-[18px] flex h-[70px] w-[70px] items-center justify-center rounded-full bg-[#D6D6D6]">
+        <svg
+          width="22"
+          height="17"
+          viewBox="0 0 22 17"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.73281 17L0.132812 9.40001L2.03281 7.50001L7.73281 13.2L19.9661 0.966675L21.8661 2.86667L7.73281 17Z"
+            fill="#333333"
+          />
+        </svg>
+      </div>
+      <p className="mb-2 text-center text-[20px] text-[#333333] md:text-[24px]">
+        You are all set!
+      </p>
+      <p className="text-base text-[#6D6D6D] md:text-[20px]">
+        Your accounting workspace is ready. start tracking sales, expenses,
+        customer payments, and vendor bills - and reconcile your bank account
+        with just a few clicks.
+      </p>
+      <div className="mt-7 w-full self-start">
+        <p className="mb-4 text-[20px] font-semibold md:text-[24px]">
+          What&apos;s next?
+        </p>
+        <div className="flex items-center gap-3">
+          <svg
+            width="13"
+            height="20"
+            viewBox="0 0 13 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.025 20L0.25 18.225L8.475 10L0.25 1.775L2.025 0L12.025 10L2.025 20Z"
+              fill="#5F6368"
+            />
+          </svg>
+          <p className="text-base">
+            Add your first sale, expense or vendor bill
+          </p>
+        </div>
+        <div className="mt-3 flex items-center gap-3">
+          <svg
+            width="13"
+            height="20"
+            viewBox="0 0 13 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.025 20L0.25 18.225L8.475 10L0.25 1.775L2.025 0L12.025 10L2.025 20Z"
+              fill="#5F6368"
+            />
+          </svg>
+          <p className="text-base">
+            Upload a bank statement to start reconciling
+          </p>
+        </div>
+        <div className="mt-3 flex items-center gap-3">
+          <svg
+            width="13"
+            height="20"
+            viewBox="0 0 13 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.025 20L0.25 18.225L8.475 10L0.25 1.775L2.025 0L12.025 10L2.025 20Z"
+              fill="#5F6368"
+            />
+          </svg>
+          <p className="text-base">
+            Set up your chart of accounts (categories)
+          </p>
+        </div>
+        <div className="mt-3 flex items-center gap-3">
+          <svg
+            width="13"
+            height="20"
+            viewBox="0 0 13 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.025 20L0.25 18.225L8.475 10L0.25 1.775L2.025 0L12.025 10L2.025 20Z"
+              fill="#5F6368"
+            />
+          </svg>
+          <p className="text-base">Review your ledgers</p>
+        </div>
+      </div>
+      <div className="mt-[31px] w-full">
+        <Button className="w-full" onClick={() => router.push('/dashboard')}>
+          Go to Dashboard
+        </Button>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/onboarding/onboarding.tsx
@@ -0,0 +1,111 @@
+'use client'
+import OnboardingProgress from './OnboardingProgress'
+import { useState } from 'react'
+import BasicsStep from './BasicsStep'
+import BankStep from './BankStep'
+import LedgerStep from './LedgerStep'
+import FinishStep from './FinishStep'
+
+export default function Onboarding() {
+  const [currentStep, setCurrentStep] = useState(1)
+  interface FormData {
+    // Basics step
+    businessName: string
+    businessType: string
+    reportingYear: string
+    currency: string
+
+    // Bank step
+    bankName: string
+    accountName: string
+    accountNumber: string
+    openingCashBalance: string
+
+    // Ledger step
+    generalLedger: boolean
+    vendorLedger: boolean
+    customerLedger: boolean
+  }
+  const [formData, setFormData] = useState<FormData>({
+    // Basics step
+    businessName: '',
+    businessType: '',
+    reportingYear: '',
+    currency: '',
+
+    // Bank step
+    bankName: '',
+    accountName: '',
+    accountNumber: '',
+    openingCashBalance: '',
+
+    // Ledger step
+    generalLedger: true,
+    vendorLedger: false,
+    customerLedger: false,
+  })
+
+  const handleNext = () => {
+    setCurrentStep(currentStep + 1)
+  }
+
+  const handleSkip = () => {
+    setCurrentStep(currentStep - 1)
+  }
+
+  const handleInputChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value, type, checked } = e.target as HTMLInputElement
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'checkbox' ? checked : value,
+    }))
+  }
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 1:
+        return (
+          <BasicsStep
+            formData={formData}
+            handleInputChange={handleInputChange}
+            handleNext={handleNext}
+          />
+        )
+      case 2:
+        return (
+          <BankStep
+            formData={formData}
+            handleInputChange={handleInputChange}
+            handleNext={handleNext}
+            handleSkip={handleSkip}
+          />
+        )
+      case 3:
+        return (
+          <LedgerStep
+            formData={formData}
+            handleInputChange={handleInputChange}
+            handleNext={handleNext}
+            handleSkip={handleSkip}
+          />
+        )
+      case 4:
+        return <FinishStep />
+      default:
+        return null
+    }
+  }
+
+  return (
+    <div className="mx-auto my-4 flex min-h-screen max-w-[714px] flex-col justify-center p-5">
+      <OnboardingProgress currentStep={currentStep} />
+      <main className="w-full">
+        <div className="w-full">{renderStep()}</div>
+      </main>
+    </div>
+  )
+}
