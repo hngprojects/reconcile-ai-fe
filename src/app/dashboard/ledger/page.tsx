c07157dbd0ac64f467977a780a961a4ef20bff1d
Muogbo Emmanuel <muogboemmanuel9@gmail.com>
2025-04-04 03:30:24 +0100

    refactor: dashboard and ledger refactor

--- src/app/dashboard/ledger/page.tsx
+++ src/app/dashboard/ledger/page.tsx
@@ -4,7 +4,7 @@ import SiteLoader from '@/components/site-loader'
 import { getQueryClient } from '@/actions/get-query-client'
 import { get_reconcilations } from '@/actions/reconcilation-server'
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
-import { Ledger } from '@/components/ledgers/Ledger'
+import { Ledger } from '@/components/dashboard/ledgers/Ledger'
 
 export default async function ReconciliationPage() {
   const queryClient = getQueryClient()
