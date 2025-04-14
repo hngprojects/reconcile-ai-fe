41d02d1c94704d8b21cefffa6b4250de5dbc2357
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-04-14 02:26:17 +0100

    fix: account settings formItems layout shift caused by zod validation

--- src/components/dashboard/settings/AccountTab.tsx
+++ src/components/dashboard/settings/AccountTab.tsx
@@ -58,7 +58,7 @@ export function AccountTab() {
                 control={form.control}
                 name="fiscalYear"
                 render={({ field }) => (
-                  <FormItem>
+                  <FormItem className="h-fit">
                     <FormLabel className="font-normal">Fiscal Year</FormLabel>
                     <Select
                       onValueChange={field.onChange}
@@ -86,7 +86,7 @@ export function AccountTab() {
                 control={form.control}
                 name="currency"
                 render={({ field }) => (
-                  <FormItem>
+                  <FormItem className="h-fit">
                     <FormLabel className="font-normal">
                       Default Currency
                     </FormLabel>
