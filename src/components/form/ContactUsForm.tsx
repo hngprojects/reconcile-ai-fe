81677fc53a5db2992ab94d78a3090d83ab57d521
Elijah Victor <elijahvix695@gmail.com>
2025-04-15 10:33:46 +0100

    Revert "chore(api): move client-side API request to server-side"

--- src/components/form/ContactUsForm.tsx
+++ src/components/form/ContactUsForm.tsx
@@ -16,7 +16,7 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import { handleContactUs } from '@/actions/api'
+import { handleContactUs } from '@/lib/api'
 import {
   Select,
   SelectContent,
